00cf373c6d51b2cbdcce90e10c93f2a04a6398ba
arthurfagundes <arthuurfagundes@gmail.com>
2022-10-25 20:16:22 -0300

    iCook App

---
+++ app/src/main/java/br/ulbra/icookapp/MainActivity.java
@@ -0,0 +1,27 @@
+package br.ulbra.icookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btRegistrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btRegistrar = (Button) findViewById(R.id.btRegistrar);
+        btRegistrar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FormCadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
